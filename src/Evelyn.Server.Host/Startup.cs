fa5fd5ca5fc6b34009bf639f8bbd6802696c1a9a
Philip Wood <philip.m.wood@gmail.com>
2018-05-30 19:56:15 +0100

    Update packages to 2.1. Create ew project for sample server host .

---
+++ src/Evelyn.Server.Host/Startup.cs
@@ -0,0 +1,80 @@
+﻿namespace Evelyn.Server.Host
+{
+    using Evelyn.Core.WriteModel;
+    using Evelyn.Management.Api.Rest;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
+
+            services.AddEvelynApi(api =>
+            {
+                api.WithWriteModel(wm =>
+                {
+                    // Use a simple in-memory event store...
+                    wm.WithEventStore.InMemory(es =>
+                    {
+                        es.WithEventPublisher.SynchronouslyInProcess();
+                    });
+
+                    // ...or,  use Greg Young's EventStore
+                    ////wm.WithEventStore.UsingEventStoreDotOrg(es =>
+                    ////{
+                    ////    es.ConnectionFactory = new EventStoreConnectionFactory("tcp://eventstore:1113");
+                    ////    es.WithEventPublisher.RunningInBackgroundService(p =>
+                    ////    {
+                    ////        p.PublishEvents.SynchronouslyInProcess();
+                    ////    });
+                    ////});
+                });
+
+                api.WithReadModel(rm =>
+                {
+                    rm.WithReadStrategy.ReadFromCache(c => c.InMemoryCache());
+                });
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IStartUpCommands startUpCommands)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            //app.UseHsts();
+            //app.UseHttpsRedirection();
+            app.UseMvc();
+
+            app.UseEvelynApi(startUpCommands);
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v0.1/swagger.json", "Evelyn Management API"));
+
+        }
+    }
+}
