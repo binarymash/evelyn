132da5f8c9b982063762cebab28f3ba8e5473c2a
Philip Wood <philip.m.wood@gmail.com>
2018-05-31 15:06:44 +0100

    Oops - forgot about this class...

---
+++ src/Evelyn.Server.Host/EventStoreConnectionFactory.cs
@@ -0,0 +1,29 @@
+﻿namespace Evelyn.Server.Host
+{
+    using System;
+    using EventStore.ClientAPI;
+    using Storage.EventStore;
+
+    public class EventStoreConnectionFactory : IEventStoreConnectionFactory
+    {
+        private readonly ConnectionSettings _connectionSettings;
+        private readonly Uri _uri;
+        private readonly string _connectionName;
+
+        public EventStoreConnectionFactory(string uri, string connectionName = "Evelyn.Server.Host")
+        {
+            _connectionSettings = ConnectionSettings.Create()
+                .EnableVerboseLogging()
+                .UseConsoleLogger()
+                .KeepReconnecting()
+                .Build();
+            _uri = new Uri(uri);
+            _connectionName = connectionName;
+        }
+
+        public IEventStoreConnection Invoke()
+        {
+            return EventStoreConnection.Create(_connectionSettings, _uri, _connectionName);
+        }
+    }
+}
